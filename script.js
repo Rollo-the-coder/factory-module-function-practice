f1a9ea1b78cc0dc869a09c4b524f33c38fa60502
Rollo-the-coder <erikgotta@gmail.com>
2020-12-09 13:53:35 +0700

    Module Pattern

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
+//factory function for character
 const characterFactory = (name, element) => {
     const sayChoice = () => console.log('You Choose');
     return {name, element, sayChoice}
 };
+
+//factory function for weapon
+const weaponFactory = (name, damage, element) => {
+    const multiplyer = () => {if (element == "Fire") {
+        return (damage * 2);
+    } else if (element == "Hydro") {
+        return (damage * 1.5);
+        } else {
+            return (damage*1)
+        }
+    }
+    return {name, damage, element, multiplyer};
+};
+
+
+//Maybe need to just create one factory with all of this inside of it?
+//Damage calculator factory
+// const damageFactory = enemy => {
+//     if (lev)
+// }
+
+
 
 const keqing = characterFactory('Keqing', 'Electro');
 
@@ -19,19 +42,6 @@ console.log(diluc);
 const childe = characterFactory('Childe', 'Hydro');
 console.log(childe);
 
-
-const weaponFactory = (name, damage, element) => {
-    const multiplyer = () => {if (element == "Fire") {
-        return (damage * 2);
-    } else if (element == "Hydro") {
-        return (damage * 1.5);
-        } else {
-            return (damage*1)
-        }
-    }
-    return {name, damage, element, multiplyer};
-};
-
 const battlePassBow = weaponFactory('bp bow', 100, "Fire");
 console.log(battlePassBow);
 console.log(battlePassBow.multiplyer());
@@ -39,3 +49,18 @@ console.log(battlePassBow.multiplyer());
 const blacksword = weaponFactory("black sword", 150, "Electro");
 console.log(blacksword.name);
 console.log(blacksword.multiplyer());
+
+
+const modulePatternTest = (() => {
+const damage = (a, b) => a + b;
+const number = () => {
+    console.log("Hello module test")
+};
+
+return {
+    damage, number,
+};
+})();
+
+modulePatternTest.damage(2,2);
+modulePatternTest.number();
